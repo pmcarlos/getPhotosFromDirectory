34543e51fabd1c4b77d2821407296cec9a856cd0
Carlos Peña <carlos@passaromx.com>
2018-01-25 18:34:06 -0600

    changed to get photo

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 
+const express = require('express');
+const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const PhotosController = require('./controllers/photos');
 
@@ -6,44 +8,61 @@ mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://movilidad:passaro2017@ds115758.mlab.com:15758/fotosmovilidad', {
     //useMongoClient: true
 });
+const app = express();
 
-var fs = require('fs');
- 
-if (process.argv.length <= 2) {
-    console.log("Usage: " + __filename + " path/to/directory");
-    process.exit(-1);
-}
- 
-var path = process.argv[2];
-fs.readdir(path, function(err, items) {
-  const total = items.length;
-
-  for (var i=0; i<items.length; i++) {
-    var file = path + '/' + items[i];
-    let count = i+1;
-    var date = '';
-    let imei = items[i].split('_');
-    let photo = items[i];
-    if(imei[0]=='i') {
-        imei = imei[1];
-    } else {
-        imei = imei[0];
-    }
-
-    imei = imei.slice(0,14);
-
-    fs.stat(file, function(f) {
-      return function(err, stats) {
-        let doc = {
-          photo: photo,
-          imei: imei,
-          date: stats['birthtime']
-        };
-        
-        PhotosController.save(doc, count, total);        
-      }
-    }(file));
-  }
-
+const photosRoute = require('./routes/photos');
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, PATCH");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  next();
 });
 
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true, parameterLimit: 10000000, limit: '50mb' }));
+
+app.use('/photos', photosRoute);
+
+// var fs = require('fs');
+ 
+// if (process.argv.length <= 2) {
+//     console.log("Usage: " + __filename + " path/to/directory");
+//     process.exit(-1);
+// }
+ 
+// var path = process.argv[2];
+// fs.readdir(path, function(err, items) {
+//   const total = items.length;
+
+//   for (var i=0; i<items.length; i++) {
+//     var file = path + '/' + items[i];
+//     let count = i+1;
+//     var date = '';
+//     let imei = items[i].split('_');
+//     let photo = items[i];
+//     if(imei[0]=='i') {
+//         imei = imei[1];
+//     } else {
+//         imei = imei[0];
+//     }
+
+//     imei = imei.slice(0,14);
+
+//     fs.stat(file, function(f) {
+//       return function(err, stats) {
+//         let doc = {
+//           photo: photo,
+//           imei: imei,
+//           date: stats['birthtime']
+//         };
+        
+//         PhotosController.save(doc, count, total);        
+//       }
+//     }(file));
+//   }
+
+// });
+//start server
+const port = process.env.PORT  || 3001;
+app.listen(port, () => console.log(`Server is listening on port ${port}`));
--- controllers/photos.js
+++ controllers/photos.js
@@ -1,4 +1,5 @@
 const Photo = require('../models/photo');
+const request = require('request');
 
 module.exports = {
     save:  (doc, count, total) => {
@@ -10,6 +11,23 @@ module.exports = {
           }, function(err) {
             console.log('Photo not saved #' + count + ' of ' + total);
           });        
-    }
+    },
+
+    getPhoto: async (req, res, next) => {
+        console.log('getphoto');
+        const {imei} = req.params;
+        const date = new Date(req.params.date);
+
+        const photo = await Photo.findOne({imei: imei, date: {'$lte':date}}).sort({date: 'desc'});
+
+        if(!photo) 
+            error('No existe',
+                `No existe una photo para el imei: ${req.params.imei} en la fecha ${req.params.date}`,404); 
+
+        const response = [photo, date];
+
+        request('http://drongeic.mx:8080/movilidad/uploads/'+photo.photo).pipe(res);
+        
+    },
 
 }
